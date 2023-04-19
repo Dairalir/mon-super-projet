fb9e41d63d1b1b4c0804a56fe70fdd88d4385fe3
Dairalir <daimanvm@gmail.com>
2023-04-19 16:48:11 +0200

    init fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Rubrique;
+use App\Entity\SousRubrique;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,20 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $rub1 = new Rubrique();
+        $rub1->setName('Groupes');
+    
+        $manager->persist($rub1);
+
+        $srub1 = new SousRubrique;
+        $srub1->setName('Dreamcatcher');
+        $srub1->setPicture('dreamcatcherlogo.jpeg');
+
+        $srub1->setRubrique($rub1);
+
+        $manager->persist($srub1);
+
+
 
         $manager->flush();
     }
