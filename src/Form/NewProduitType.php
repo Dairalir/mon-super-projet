31c2c72737d46f77ed8c377f1774c73f9eb56a31
Dairalir <daimanvm@gmail.com>
2023-04-20 14:26:32 +0200

    creation builder formulaire

---
+++ src/Form/NewProduitType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Produit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewProduitType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('prix')
+            ->add('picture')
+            ->add('stock')
+            ->add('active')
+            ->add('sous_rubrique')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Produit::class,
+        ]);
+    }
+}
