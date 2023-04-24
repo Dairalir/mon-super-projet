3a60abb87aa259528c1f7192e1046a05a36fbe55
Dairalir <daimanvm@gmail.com>
2023-04-24 16:43:18 +0200

    creation de vue de form

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Produit;
+use App\Form\NewProduitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+
+#[Route('/produit')]
 class ProduitController extends AbstractController
 {
-    #[Route('/produit', name: 'produit')]
+    #[Route('/', name: 'produit')]
     public function index(): Response
     {
         return $this->render('produit/index.html.twig', [
@@ -18,12 +21,13 @@ class ProduitController extends AbstractController
         ]);
     }
 
+    #[Route('/new', name: 'produit_new', methods: ['GET', 'POST'])]
     public function new(Request $request): Response
 
     {
     $produit = new Produit();
     // création du formulaire
-    $form = $this->createForm(ProductsType::class, $produit);
+    $form = $this->createForm(NewProduitType::class, $produit);
     // lecture du formulaire
     $form->handleRequest($request);
     return $this->render('produit/new.html.twig', [
