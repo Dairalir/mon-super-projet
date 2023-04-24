1a9cf3dfec5b0661cd96c4f21d955c759b2dd7e5
Dairalir <daimanvm@gmail.com>
2023-04-24 16:43:03 +0200

    modif builder form

--- src/Form/NewProduitType.php
+++ src/Form/NewProduitType.php
@@ -3,22 +3,64 @@
 namespace App\Form;
 
 use App\Entity\Produit;
+use App\Entity\SousRubrique;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class NewProduitType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('prix')
-            ->add('picture')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du produit',
+                'attr' => [
+                    'placeholder' => 'Produit',
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description',
+                'attr' => [
+                    'placeholder' => 'Description',
+                ],
+            ])
+            ->add('prix', MoneyType::class, [
+                'label' => 'Prix',
+                'attr' => [
+                    'placeholder' => 'Prix',
+                ],
+            ])
+            ->add('picture', FileType::class,[
+                'label' => 'Image du produit',
+                'help' => 'Selectionnez une image',
+                'attr' => [
+                    'placeholder' => 'image.jpg',
+                ],
+            ])
             ->add('stock')
             ->add('active')
-            ->add('sous_rubrique')
+            //->add('sous_rubrique', EntityType::class, [
+            //    'class' => SousRubrique::class,
+            //    'choice_label' => 'name',
+            //    //'choice_value' => 'name',
+            //    'multiple' => true,
+            //])
+            ->add('sous_rubrique', EntityType::class, [
+                'class' => SousRubrique::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('sous_rubrique')
+                        ->orderBy('sous_rubrique.name', 'ASC');
+                },
+                'choice_label' => 'name',
+                'multiple' => true,
+            ])
         ;
     }
 
